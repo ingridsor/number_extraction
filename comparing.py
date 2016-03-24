ed89b4843299add575de789ff5184f653035dc94
ingridsor <ingrid.e.sor@gmail.com>
2016-03-24 11:52:02 +0100

    minor changes

--- comparing.py
+++ comparing.py
@@ -1,17 +1,18 @@
-import sys, difflib
+import sys, difflib, re
 
+# Result file as first argument, gold file as second
 fileA = sys.argv[1]
 fileB = sys.argv[2]
 
-diff_count = 0
-num_count = 0
-
+num_count = 0  # Counts number of lines, i.e. messages
+sugg_nr = []   # Counts where numbers have been suggested
+    
 with open(fileA, 'r') as file1:
     with open(fileB, 'r') as file2:
         same = set(file1).intersection(file2)
         
 with open(fileA,'r') as file1:
     num_count = len(file1.readlines())
-
+    
 perc_corr = 100 * len(same) / num_count
-print('Percentage correct: ', perc_corr)
+print('Total percentage correct: ', perc_corr)
